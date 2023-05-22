a3d83b9a391bf121f980ab27ac0ae38e29931d8f
Sheerwin <sheerwin2002@hotmail.com>
2023-05-22 23:08:06 +0800

    Binary Search Modified.

--- DSTR_Assignment/src/Search.h
+++ DSTR_Assignment/src/Search.h
@@ -7,19 +7,41 @@
 #include "LinkedList.h"
 using namespace std;
 
-//binary search
 template <class T>
+//Needed to be sorted first, the code not include any sorting algorithm
 LinkedList<T>* binarySearch(LinkedList<T>* list, string key, int index) {
-	LinkedList<T>* result = new LinkedList<T>();
-	node<T>* tmp = list->head;
-	int counter = 0;
-	while (tmp != NULL) {
-		if (tmp->data[index] == key) {
-			result->insertAtEnd(tmp->data);
-			counter++;
-		}
-		tmp = tmp->nextAddress;
-	}
-	if (counter == 0) return NULL;
-	return result;
-};
+    if (list == nullptr || list->head == nullptr) {
+        return nullptr;  // Empty list, return nullptr
+    }
+
+    if (index < 0 || index >= list->head->data.size()) {
+        return nullptr;  // Invalid index, return nullptr
+    }
+
+    LinkedList<T>* result = nullptr;
+    node<T>* left = list->head;
+    node<T>* right = nullptr;
+
+    while (left != right) {
+        node<T>* slow = left;
+        node<T>* fast = left->nextAddress;
+        while (fast != right && fast->nextAddress != right) {
+            slow = slow->nextAddress;
+            fast = fast->nextAddress->nextAddress;
+        }
+
+        if (slow->data[index] == key) {
+            result = new LinkedList<T>();
+            result->insertAtEnd(slow->data);
+            return result;
+        }
+        else if (slow->data[index] < key) {
+            left = slow->nextAddress;
+        }
+        else {
+            right = slow;
+        }
+    }
+
+    return result;
+}
