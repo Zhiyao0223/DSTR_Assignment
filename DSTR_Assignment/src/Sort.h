03e5e811ce4d07c0ccb68fc83d65054945e3c755
LAPTOP-6VGLBI88\User <brlkflee@gmail.com>
2023-05-25 16:43:13 +0800

    complete sort

--- DSTR_Assignment/src/Sort.h
+++ DSTR_Assignment/src/Sort.h
@@ -3,12 +3,30 @@
 #include <iostream>
 #include "FileIO.h"
 #include "University.h"
-#include "LinkedList.h"`
+#include "LinkedList.h"
 using namespace std;
 
 template <class T>
-void selectionSort() {
-	FileIO file;
-	LinkedList<University>* uniList = file.readFile();
-	uniList
+void selectionSort(LinkedList<T>* list, string key, int index) {
+    FileIO file;
+    LinkedList<University>* uniList = file.readFile();
+    node<T>* current = list->head;
+
+    while(current != nullptr) {
+        node<T>* minNode = current;
+        node<T>* nextNode = current->nextAddress;
+
+        while (nextNode != nullptr) {
+            if (nextNode->nextAddress < minNode->data) {
+                minNode = nextNode;
+            }
+            nextNode = nextNode->nextAddress;
+        }
+
+        if (minNode != current) {
+            std::swap(minNode->data, current->data);
+        }
+
+        current = current->nextAddress;
+    }
 }
